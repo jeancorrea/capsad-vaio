320cfd4d2e0fe9c3257e7accea241a519ae49b8f
Jean Correa <jean.do.violao@gmail.com>
2015-01-28 22:52:45 -0200

    Alterações feitas na Zumbi 002

--- do_proced.php
+++ do_proced.php
@@ -14,15 +14,32 @@ $data = $dp_a."-".$dp_m."-".$dp_d;
 $membros = $_POST['membros'];
 $array = explode(", ", $membros);
 $size = sizeof($array) - 1;
+
+$sqlA = mysql_query("SELECT * FROM procedimentos WHERE procedimento = '$procedimento'");
+while ($linha = mysql_fetch_array($sqlA)) {
+	$codigo = $linha['codigo'];
+}
 for ($x = 0; $x<$size; $x++) {
 	$membro = $array[$x];
-
-	echo $procedimento."<br />";
-	echo $prof.", ".$outros."<br />";
+	$membro = str_replace("'","\\'",$membro);
+	
+	/*echo $procedimento."<br />";
+	echo $codigo."<br />";
+	echo $prof."<br />";
+	echo $outros."<br />";
 	echo $data."<br />";
-	echo $membro."<br />";
-	echo "<br /><br />";
-}
+	echo $membro."<br /><br />";*/
+
+	
+	$sqlB = mysql_query("INSERT INTO atendimentos (atendimento, codigo, profissionais, outros, data, paciente) VALUES ('$procedimento', '$codigo', '$prof', '$outros', '$data', '$membro')");
+	}
+	if ($sqlB == true) {
+		echo "Dados cadastrados com sucesso!";
+	} else {
+		echo "Não foi possível cadastrar os dados. Favor contatar o administrador do banco de dados.";
+	}
+	
+
 
 ?>
 <?php include "footer.php"; ?>
